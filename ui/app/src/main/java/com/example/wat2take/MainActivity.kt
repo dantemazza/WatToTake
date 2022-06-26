4da2053c2415c7bf2a0859ccb31ea101a949e3a9
Sathira Katugaha <sghmk12@users.noreply.github.com>
2022-06-25 21:36:55 -0400

    Added Course Information Data Store (#4)
    * JSON to Course converter, pref datastore dep added
    
    * Use array list of courses in datastore instead of individual
    
    * Objects back to JSON function
    
    * Preliminary MVVM
    
    * Revert "Preliminary MVVM"
    
    This reverts commit 944730df2253a22a17116035d98b7232686a6fb7.
    
    * Added data class and prelim data store
    
    * Moved saving to mainactivity
    
    * Update MainActivity.kt
    
    * fixed default value of datastore
    
    * removed hard code
    
    * changed navigation method to default compose navigation
    
    Co-authored-by: Richard <rl2zhang@uwaterloo.ca>
    Co-authored-by: jinwuliu <liujinwu2@gmail.com>
    Co-authored-by: Sathira Katugaha <SGKat@SathBook-Pro.local>

--- ui/app/src/main/java/com/example/wat2take/MainActivity.kt
+++ ui/app/src/main/java/com/example/wat2take/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.wat2take
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.wat2take.ui.theme.Wat2TakeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,41 +14,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Wat2TakeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column() {
-                        Greeting("Android")
-                        MyCoursesButton()
-                    }
-                }
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun MyCoursesButton() {
-    val context = LocalContext.current
-
-    Button(onClick = {
-        context.startActivity(Intent(context, MyCoursesActivity::class.java))
-    }) {
-        Text(text = "My Courses")
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Wat2TakeTheme {
-        Greeting("Android")
+fun Main() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { Home(navController) }
+        composable("myCourses") { MyCoursesList(navController) }
     }
 }
---
+++ ui/app/src/main/java/com/example/wat2take/MyCourses.kt
@@ -0,0 +1,87 @@
+package com.example.wat2take
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.wat2take.data.Course
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+
+@Composable
+fun MyCoursesList(navController: NavController) {
+    val context = LocalContext.current
+    val dataStore = TranscriptDataStore(context)
+    var courseListJson = dataStore.getCourseList.collectAsState(
+        initial = TranscriptDataStore.DEFAULT_COURSES_VAL
+    ).value;
+
+    val courses = parseCourseListJSON(courseListJson)
+
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = courses,
+            itemContent = {
+                CourseListItem(course = it)
+            }
+        )
+    }
+}
+
+@Composable
+fun CourseListItem(course: Course) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        elevation = 2.dp,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ) {
+        Row() {
+            Column (modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterVertically)
+                .weight(2f)
+            ) {
+                Text(text = course.name, style = typography.h6)
+                Text(text = course.title, style = typography.caption)
+            }
+            Column(modifier = Modifier
+                .padding(16.dp)
+                .weight(1f),
+                horizontalAlignment = Alignment.End
+
+            ) {
+                Text(text = "Grade: " + course.grade, style = typography.body1)
+            }
+        }
+    }
+}
+
+fun parseCourseListJSON(json: String): List<Course> {
+    val gson = Gson()
+    val type: Type = object : TypeToken<List<Course?>?>() {}.type
+    val courseList: List<Course> = gson.fromJson(json, type)
+    return courseList
+}
+
+fun courseListToJSON(courses: List<Course>): String {
+    val gson = Gson()
+    val type: Type = object : TypeToken<List<Course?>?>() {}.type
+    val json = gson.toJson(courses, type)
+    return json
+}
