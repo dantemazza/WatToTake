fbb5393a0fd2ce96ec3150cf7db207772e474872
Richard <rl2zhang@uwaterloo.ca>
2022-06-27 12:15:02 -0400

    HTTP3 upload hacky version

--- ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
+++ ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
@@ -1,6 +1,9 @@
 package com.example.wat2take
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,21 +21,42 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.LifecycleOwner
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
-import net.gotev.uploadservice.observer.request.RequestObserver
 import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.io.IOException
+
 
 @Composable
 fun UploadTranscript(navController: NavController) {
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission Accepted: Do something
+            Log.d("ExampleScreen","PERMISSION GRANTED")
+
+        } else {
+            // Permission Denied: Do something
+            Log.d("ExampleScreen","PERMISSION DENIED")
+        }
+    }
+
     val context = LocalContext.current
     val pickFileLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.OpenDocument()
     ) { fileUri ->
         if (fileUri != null) {
             // Update the state with the Uri
+            val filePath = fileUri.path ?: "null"
+            Log.i("PATH: ", filePath )
             Log.i("URI", fileUri.toString())
-            onFilePicked(fileUri.toString(), context)
+            sendFile("/sdcard/Download/SSR_TSRPT.pdf")
+            // onFilePicked(fileUri.toString(), context)
             navController.navigate("myCourses")
         }
     }
@@ -57,8 +81,47 @@ fun UploadTranscript(navController: NavController) {
             }) {
                 Text(text = "Choose File", fontSize = 18.sp)
             }
+            Button(onClick = {
+                launcher.launch(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+            }) {
+                Text(text = "Request Permissions", fontSize = 18.sp)
+            }
         }
     }
+}
+
+fun sendFile(filePath: String) {
+    val client = OkHttpClient()
+    val file = File(filePath)
+    val serverUrl = "https://7cac-192-159-178-206.ngrok.io//transcript"
+    // val serverUrl = "https://wattotake.herokuapp.com/transcript"
+    val request = Request.Builder()
+        .url(serverUrl)
+        // .post(RequestBody.create("text/html".toMediaType(), "hello"))
+        .post(file.asRequestBody("application/pdf; charset=utf-8".toMediaType()))
+        .build()
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            e.printStackTrace()
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            response.use {
+                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                for ((name, value) in response.headers) {
+                    println("$name: $value")
+                }
+
+                Log.i("RESPONSE", response.body!!.string())
+            }
+        }
+    })
+}
+
+private fun sendRequestBody(url: String, requestBody: RequestBody) {
+
 }
 
 fun onFilePicked(filePath: String, context: Context) {
