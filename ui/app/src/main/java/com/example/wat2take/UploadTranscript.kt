cd4d624cc33718a61356558ee9e3ca2b42526766
Richard <rl2zhang@uwaterloo.ca>
2022-07-15 14:48:04 -0400

    File path workaround

--- ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
+++ ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
@@ -1,10 +1,13 @@
 package com.example.wat2take
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Environment
+import android.provider.OpenableColumns
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
@@ -21,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
 import okhttp3.*
@@ -29,6 +31,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import java.io.IOException
+import java.util.*
 
 
 @Composable
@@ -47,19 +50,33 @@ fun UploadTranscript(navController: NavController) {
     }
 
     val context = LocalContext.current
+
+
     val pickFileLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.OpenDocument()
     ) { fileUri ->
         if (fileUri != null) {
             // Update the state with the Uri
-            val filePath = fileUri.path ?: "null"
-            Log.i("PATH: ", filePath )
+            val filePathUri = fileUri.path ?: "null"
+            Log.i("PATH: ", filePathUri )
             Log.i("URI", fileUri.toString())
-            sendFile("/sdcard/Download/SSR_TSRPT.pdf")
-            // onFilePicked(fileUri.toString(), context)
+
+            val storagePath = Environment.getExternalStorageDirectory().getPath()
+            Log.i("Testing path", "Env path: $storagePath")
+
+            val fileName = getFileData(fileUri, context)
+            if (fileName != "") { // "" Is returned if file name not found from getFileData()
+                Log.i("Update", "Received file name: $fileName")
+                val filePath = "/sdcard/Download/$fileName"
+                Log.i("Update", "File Path: $filePath")
+                sendFile(filePath)
+            } else {
+                // Toast file not found
+            }
             navController.navigate("myCourses")
         }
     }
+
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -90,16 +107,57 @@ fun UploadTranscript(navController: NavController) {
     }
 }
 
+fun getFileData(uri: Uri, context: Context) : String {
+    val contentResolver = context.contentResolver
+
+    // The query, because it only applies to a single document, returns only
+    // one row. There's no need to filter, sort, or select fields,
+    // because we want all fields for one document.
+    val cursor: Cursor? = contentResolver.query(
+        uri, null, null, null, null, null)
+
+    cursor?.use {
+        // moveToFirst() returns false if the cursor has 0 rows. Very handy for
+        // "if there's anything to look at, look at it" conditionals.
+        if (it.moveToFirst()) {
+
+            val columnNames = it.columnNames
+            Log.i("Upload", "Column Names: ${Arrays.toString(columnNames)}")
+            // Note it's called "Display Name". This is
+            // provider-specific, and might not necessarily be the file name.\
+            val displayNameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (displayNameIndex != -1) {
+                val displayName: String =
+                    it.getString(displayNameIndex)
+                Log.i("UPLOAD", "Display Name: $displayName")
+                return displayName
+            } else {
+                Log.i("Upload", "No display Name")
+            }
+        }
+    }
+    return ""
+}
+
 fun sendFile(filePath: String) {
     val client = OkHttpClient()
+
     val file = File(filePath)
-    val serverUrl = "https://7cac-192-159-178-206.ngrok.io//transcript"
-    // val serverUrl = "https://wattotake.herokuapp.com/transcript"
+//    val serverUrl = "https://7cac-192-159-178-206.ngrok.io//transcript"
+    val serverUrl = "https://wattotake.herokuapp.com/transcript"
+
+
+    val formBody = FormBody.Builder()
+        .add("file", filePath)
+        .build()
     val request = Request.Builder()
         .url(serverUrl)
-        // .post(RequestBody.create("text/html".toMediaType(), "hello"))
-        .post(file.asRequestBody("application/pdf; charset=utf-8".toMediaType()))
+//        .post(file.asRequestBody("application/pdf; charset=utf-8".toMediaType()))
+        .post(formBody)
+        .addHeader("content-type", "multipart/form-data")
         .build()
+
+    val httpInterceptor = Interceptor
 
     client.newCall(request).enqueue(object : Callback {
         override fun onFailure(call: Call, e: IOException) {
@@ -123,13 +181,3 @@ fun sendFile(filePath: String) {
 private fun sendRequestBody(url: String, requestBody: RequestBody) {
 
 }
-
-fun onFilePicked(filePath: String, context: Context) {
-    MultipartUploadRequest(context, serverUrl = "https://025f-192-159-178-206.ngrok.io/transcript")
-        .setMethod("POST")
-        .addFileToUpload(
-            filePath = filePath,
-            parameterName = "file"
-        ).startUpload()
-}
-
