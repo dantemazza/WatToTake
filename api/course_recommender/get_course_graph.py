4f8003d947dea9ac25477c2e96cca6de408d1420
Dante Mazza <dantemazza1@gmail.com>
2022-07-15 21:46:18 -0400

    Prereqs and Antireqs

--- api/course_recommender/get_course_graph.py
+++ api/course_recommender/get_course_graph.py
@@ -1,38 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-COURSE_LIST_URL = "https://ucalendar.uwaterloo.ca/2223/COURSE/course-ECE.html"
-COURSE_CODE_REGEX = "([A-Z]{2,5})(\/[A-Z]{2,5})* +[0-9][0-9][0-9][A-C]?"
-# def parse_TE(url):
-#     r = requests.get(url)
-#     soup = BeautifulSoup(r.content, 'html5lib')
-#     print(soup.prettify())
+import json 
 
-# def parse_NSE(url):
-#     r = requests.get(url) 
-#     soup = BeautifulSoup(r.content, 'html5lib')
-#     print(soup.prettify())
-    
-# def parse_CSE(url):
-#     r = requests.get(url)
-#     soup = BeautifulSoup(r.content, 'html5lib')
-#     soup.prettify()
-#     headers = soup.find_all(re.compile('^h4$'))
-#     print(headers[0].text)
-#     for header in headers:
-#         for sib in header.find_next_siblings():
-#             if sib.name=="h4":
-#                 break
-#             else:
-#                 print(sib.text)
-#                 print(re.find(sib.text))
-#         break
+COURSE_LIST_URL = "https://ucalendar.uwaterloo.ca/2223/COURSE/course-ECE.html"
+COURSE_CODE_REGEX = "(?:[A-Z]{2,5})(?:\/[A-Z]{2,5})* +[0-9][0-9][0-9][A-C|L]?"
+UNFORMATTED_SEPERATION_REGEX = "(?:[A-Z]{2,5}) (?:\d+)(?:{}\s*\d+(?:[A-C|L])?)*"
+COURSE_CODE_REGEX_2 = "(?:[A-Z]{2,5}) (?:\d+)(?:,\s*\d+(?:[A-C|L])?)*"
+OR_REGEX = "(?:[A-Z]{2,5}) (?:\d+)(?: (?:or)\s*\d+(?:[A-C|L])?)*"
+BRACKET_REGEX = "\(.*\)"
+
+
+def parse_seperated_course_names(regex, string):
+    courses = []
+    course_codes_2 = re.findall(regex, string)
+    for course_code in course_codes_2:
+        if ',' in course_code:
+            codes = course_code.split(",")    
+            letters = codes[0].split(" ")[0]
+            for code in codes[1:]:
+                courses.append(f"{letters}{code}")
+            courses.append(codes[0])
+    return courses
+
 
 def parse_course_list(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html5lib')
-    # print(soup.prettify())
-    #body.main.center.div.div
     courses = []
     centers = soup.find_all('center')
     divs = [c.find_all('div')[0] for c in centers]
@@ -48,28 +42,57 @@ def parse_course_list(url):
         courses.append(course)
     return courses
         
-def parse_antireqs(string):
-    string = string.split(":")[1].strip()
-    print(string)
+def parse_antireqs(string, split=True):
+    if split:
+        string = string.split(":")[1].strip()
+    course_codes = re.findall(COURSE_CODE_REGEX, string)
+
+    courses = []
+    for course_code in course_codes:
+        if '/' in course_code:
+            codes = course_code.split('/')
+            num = codes[-1].split(" ")[-1]
+            for code in codes[:-1]:
+                courses.append(f"{code} {num}")
+            courses.append(codes[-1])
+        else:
+            courses.append(course_code)
+    courses += parse_seperated_course_names(COURSE_CODE_REGEX_2, string)
+    courses = list(set(courses))
+    return courses
 
 def parse_prereqs(string):
     string = string.split(":")[1].strip()
-    print(string)
+    courses = parse_antireqs(string, split=False)
+    ourses = parse_antireqs(string, split=False)
+    ourses_2 = parse_seperated_course_names(OR_REGEX, string)
+    or_courses = set(ourses+ourses_2)
+
+    courses = set(courses) - or_courses
+    res = {"or_courses": list(or_courses), "and_courses": list(courses)}
+    return res
 
 def parse_course_graph(courses):
+    antireq = {}
+    prereq = {}
     for course in courses: 
         course_code = re.findall(COURSE_CODE_REGEX, course[0])[0]
         for c in course:
             if 'Antireq' in c:
                 antireqs = parse_antireqs(c)
+                antireq[course_code] = antireqs
             if 'Prereq' in c:
-                # prereqs = parse_prereqs(c)
-                pass
-    antireq = {}
-    prereq = {}
+                prereqs = parse_prereqs(c)
+                prereq[course_code] = prereqs
+    
     return prereq, antireq
 
 
 if __name__ == '__main__':
     course_list = parse_course_list(COURSE_LIST_URL)
     prereq, antireq = parse_course_graph(course_list)
+
+    with open('json_folder/antireq.json', 'w') as f:
+        json.dump(antireq, f, indent=2)
+    with open('json_folder/prereq.json', 'w') as f:
+        json.dump(prereq, f, indent=2)
