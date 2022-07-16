7099184fc9a9e26b9fdf1e110e4a09e591d91c30
Sathira Katugaha <SGKat@SathBook-Pro.local>
2022-07-16 19:08:49 -0400

    added file permissions for android 10+

--- ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
+++ ui/app/src/main/java/com/example/wat2take/UploadTranscript.kt
@@ -4,7 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.database.Cursor
 import android.net.Uri
-import android.os.Environment
+import android.os.Build
 import android.provider.OpenableColumns
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -12,7 +12,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +21,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.google.accompanist.permissions.*
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.gson.JsonParser
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -33,7 +34,6 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 
@@ -47,16 +47,32 @@ fun UploadTranscript(navController: NavController) {
         ActivityResultContracts.GetContent()
     ) { fileUri ->
         if (fileUri != null) {
-            // Update the state with the Uri
-            val filePathUri = fileUri.path
-            Log.i("filePathUri: ", filePathUri ?: "")
+            var path = ""
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q){
+                try{
+                    val copyPath = copyFileToInternalStorage(fileUri, "", context)
+                    Log.i("copyPath", copyPath ?: "")
+                    if(copyPath != null){
+                        path = copyPath
+                    }else{
+                        Log.e("File Copy Error", "Could not copy file into local directory")
+                    }
+                }catch (e: Exception){
+                    Log.e("File Copy Error", e.toString())
+                }
+            }else{
+                val filePathUri = fileUri.path
+                Log.i("filePathUri: ", filePathUri ?: "")
 
-            if(filePathUri != null){
-                val filePath = filePathUri.replace("/document/raw:", "")
-                Log.i("filePath", filePath)
-                sendFile(filePath, dataStore)
+                if(filePathUri != null){
+                    val filePath = filePathUri.replace("/document/raw:", "")
+                    Log.i("filePath", filePath)
+                    path = filePath
+                }else{
+                    Log.e("File Path URI Error", "Could not retreive file path from URI")
+                }
             }
-
+            sendFile(path, dataStore)
             GlobalScope.launch {
                 dataStore.setLoadingKey(true)
             }
@@ -103,48 +119,45 @@ fun UploadTranscript(navController: NavController) {
             }
         }
     }
+}
 
-//    val storagePermissionsGranted = dataStore.getStoragePermissionsGranted
-//        .collectAsState(initial = false).value
-//
-//    val requestPermissionLauncher =
-//        rememberLauncherForActivityResult(
-//            ActivityResultContracts.RequestPermission()
-//        ) { isGranted: Boolean ->
-//            if (isGranted) {
-//                GlobalScope.launch {
-//                    dataStore.setStoragePermissionGranted(true)
-//                }
-//            } else {
-//                GlobalScope.launch {
-//                    dataStore.setStoragePermissionGranted(false)
-//                }
-//            }
-//        }
-//
-//    LaunchedEffect(Unit){
-//        when {
-//            ContextCompat.checkSelfPermission(
-//                context,
-//                Manifest.permission.READ_EXTERNAL_STORAGE
-//            ) == PackageManager.PERMISSION_GRANTED -> {
-//                dataStore.setStoragePermissionGranted(true)
-//            }
-//            shouldShowRequestPermissionRationale() -> {
-//                // In an educational UI, explain to the user why your app requires this
-//                // permission for a specific feature to behave as expected. In this UI,
-//                // include a "cancel" or "no thanks" button that allows the user to
-//                // continue using your app without granting the permission.
-//                showInContextUI(...)
-//            }
-//            else -> {
-//                // You can directly ask for the permission.
-//                // The registered ActivityResultCallback gets the result of this request.
-//                requestPermissionLauncher.launch(
-//                    Manifest.permission.REQUESTED_PERMISSION)
-//            }
-//        }
-//    }
+private fun copyFileToInternalStorage(uri: Uri, newDirName: String, context: Context): String? {
+    val returnCursor: Cursor = context.getContentResolver().query(
+        uri, arrayOf(
+            OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE
+        ), null, null, null
+    )!!
+
+    val nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+    val sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE)
+    returnCursor.moveToFirst()
+    val name = returnCursor.getString(nameIndex)
+    val size = java.lang.Long.toString(returnCursor.getLong(sizeIndex))
+    val output: File
+    if (newDirName != "") {
+        val dir: File = File(context.getFilesDir().toString() + "/" + newDirName)
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+        output = File(context.getFilesDir().toString() + "/" + newDirName + "/" + name)
+    } else {
+        output = File(context.getFilesDir().toString() + "/" + name)
+    }
+    try {
+        val inputStream: InputStream = context.getContentResolver().openInputStream(uri)!!
+        val outputStream = FileOutputStream(output)
+        var read = 0
+        val bufferSize = 1024
+        val buffers = ByteArray(bufferSize)
+        while (inputStream.read(buffers).also { read = it } != -1) {
+            outputStream.write(buffers, 0, read)
+        }
+        inputStream.close()
+        outputStream.close()
+    } catch (e: Exception) {
+        Log.e("File Copy Error", e.message!!)
+    }
+    return output.path
 }
 
 fun sendFile(filePath: String, dataStore: TranscriptDataStore) {
